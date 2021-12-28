99c63cf663b37f84d285b8645d271f4d0b6e1d75
HardikBhanot <hardikbhanot123@gmail.com>
2021-12-28 09:52:21 +0530

    hero almost completed

--- js/index.js
+++ js/index.js
@@ -2,8 +2,34 @@
 const burger=document.querySelector(".burger")
 const navlinks=document.querySelector("nav .nav-links")
 const links= document.querySelectorAll(".nav-links li")
+const line=document.querySelectorAll(".burger >.line")
 const nav= document.querySelector("nav")
-
+const header = document.querySelector('navbar');
+const lightLinks=document.getElementsByClassName('light-link')
+window.addEventListener('scroll', function () {
+    let scrolamount = window.scrollY;
+    if(scrolamount ==0){
+      header.style.backgroundColor="transparent";
+      for( i in lightLinks ) {
+        lightLinks[i].style.color= '#DBD8E3';
+        }
+        
+    }
+    if(scrolamount>450){
+      header.style.position = "fixed";
+      burger.style.position = "fixed";
+      line.forEach((item) =>
+        item.style.backgroundColor="#5C5470");
+      burger.style.border = "1px solid #5C5470";
+      header.style.backgroundColor="rgba(222,223,225,.75)";
+      header.style.zIndex = 25; 
+      for( i in lightLinks ) {
+      lightLinks[i].style.color= '#5C5470';
+      }
+      burger.style.backgroundColor="#5C5470"
+    }
+    
+  })
 
 let showMenu = false;
  
@@ -11,11 +37,14 @@ burger.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
-        console.log("hurraaaaaaaaaaay")
+        // for( i in lightLinks ) {
+        //     lightLinks[i].style.color= '#5C5470';
+        //     }
+        // lightLinks.style.color='#5C5470'
         burger.classList.add("open");
         navlinks.classList.add("open");
         nav.classList.add("open");
-        // links.classList.add("open");
+        
         links.forEach((item) =>
         item.classList.add("fade"));
 
